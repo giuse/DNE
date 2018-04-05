47bcf2d4743c3207ea888f71a0329fa68323f4cb
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2018-04-05 09:42:22 +0200

    Fixed debug image printing

--- tools.rb
+++ tools.rb
@@ -1,16 +1,18 @@
 
 module DNE
-  # Tools to support debug and implementation
+  # Tools to support debug and implementation.
+  # I just want to be able to call `show_image` anywhere in the code and
+  # be presented with something sensible.
   module Tools
-    WB = MachineLearningWorkbench
     def self.show_image img
-      shape = [80,70]
+      shape = nil
       to_disp = case img
-      when Numo::NArray
-        WB::Tools::Normalization.feature_scaling img,
-          from: [img.class::MIN, img.class::MAX], to: [0,1]
+      when NArray, NImage
+        img = img.cast_to NArray # in case it's NImage, need floats for the scaling
+        shape = [80, 70] # BEWARE of hardcoded values
+        WB::Tools::Normalization.feature_scaling img, to: [0,1]
       else # assume python image
-        shape = [160, 210]
+        shape = [160, 210] # BEWARE of hardcoded values
         NImage[*img.mean(2).ravel.tolist.to_a]
       end
       WB::Tools::Imaging.display to_disp,
